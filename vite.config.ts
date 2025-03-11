17568c0d9f9b567a6a868ba8a22d5cc8c1096fe8
Vitaly Pavlenko <cxielamiko@gmail.com>
2025-03-11 18:51:10 +0400

    change postfix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/startuptrack/", // This should match your repository name
+  base: "/teambuilder/", // This should match your repository name
 });
